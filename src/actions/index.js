4655750dd71ca932bbb4f8a08f930e30f8acfae1
Emerson <eemr3@yahoo.com.br>
2022-01-10 18:05:19 -0300

    feat(Actions): implementa actions de login e wallet"

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,1 +1,14 @@
 // Coloque aqui suas actions
+export const LOGIN_INFO = 'LOGIN_INFO';
+
+export const WALLET_INFO = 'WALLET_INFO';
+
+export const loginInfo = (email) => ({
+  type: LOGIN_INFO,
+  email,
+});
+
+export const walletInfo = (payload) => ({
+  type: WALLET_INFO,
+  payload,
+});
