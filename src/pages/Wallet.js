b3aab8054198aeff9793f84d331c47f917797cfb
Emerson <eemr3@yahoo.com.br>
2022-01-10 19:29:37 -0300

    build(Page Wallet): adiciona componente heade à página

--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,8 +1,38 @@
 import React from 'react';
+import Header from '../components/Header';
+import Form from '../components/Form';
+import Input from '../components/Input';
 
 class Wallet extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      value: '0',
+    };
+  }
+
+  handleInputChange = ({ target }) => {
+    this.setState({
+      value: target.value,
+    });
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { value } = this.state;
+    return (
+      <>
+        <Header />
+        <Form>
+          <Input
+            data-testid="total-field"
+            typeInput="number"
+            minValue="0"
+            valueInput={ value }
+            handleInputChange={ this.handleInputChange }
+          />
+        </Form>
+      </>
+    );
   }
 }
 
