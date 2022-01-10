262d105e7da94967858327c2e8fe0d35c2f5ba2c
Emerson <eemr3@yahoo.com.br>
2022-01-10 15:34:17 -0300

    Adciona INITIAL_STATE aos arquivos user.js e wallet.js no reducer

--- src/reducers/user.js
+++ src/reducers/user.js
@@ -1,1 +1,10 @@
 // Esse reducer será responsável por tratar as informações da pessoa usuária
+const INITIAL_STATE = {
+  user: {
+    email: '',
+  },
+};
+
+const reducerUser = (state = INITIAL_STATE, action) => state;
+
+export default reducerUser;
--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,1 +1,9 @@
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
+const INITIA_STATE = {
+  wallet: {
+    currencies: [],
+    expenses: [],
+  },
+};
+
+const reducerWallet = (state = INITIA_STATE, action) => state;
