063b9827bbf7c4bffbbd714b3c155a34891e84af
Emerson <eemr3@yahoo.com.br>
2022-01-11 00:39:53 -0300

    feat(Wallet Reducer): implementa o Reducer de Wallet

--- src/reducers/wallet.js
+++ src/reducers/wallet.js
@@ -1,11 +1,26 @@
+import { WALLET_INFO, CURRENCY_REQUEST_KEYS } from '../actions';
+
 // Esse reducer será responsável por tratar o todas as informações relacionadas as despesas
 const INITIA_STATE = {
-  wallet: {
-    currencies: [],
-    expenses: [],
-  },
+  currencies: [],
+  expenses: [],
 };
 
-const reducerWallet = (state = INITIA_STATE) => state;
+const reducerWallet = (state = INITIA_STATE, action) => {
+  switch (action.type) {
+  case WALLET_INFO:
+    return {
+      ...state,
+      expenses: [...state.expenses, action.payload, ...state.currencies],
+    };
+  case CURRENCY_REQUEST_KEYS:
+    return {
+      ...state,
+      currencies: [...action.keys],
+    };
+  default:
+    return state;
+  }
+};
 
 export default reducerWallet;
