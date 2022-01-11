344ed0fa9a0b2b116db86e7d6bf94b0b9d5a378b
Emerson <eemr3@yahoo.com.br>
2022-01-11 00:48:43 -0300

    build(Header): adicona connect ao componente e busca dados no estado global

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -2,18 +2,28 @@ import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 
+import './Header.css';
+
 class Header extends Component {
   render() {
-    const { userEmail } = this.props;
+    const { userEmail, walletExpesseTotal } = this.props;
     return (
       <header>
         <h1>TrybeWallet</h1>
-        <p data-testid="email-field">{userEmail}</p>
+        <p data-testid="email-field">
+          Olá
+          {' '}
+          {userEmail}
+        </p>
         <p>
           Total das despesas:
           {' '}
           R$
-          <span data-testid="total-field">1000,00</span>
+          <span data-testid="total-field">
+            {walletExpesseTotal.length === 0 ? '0'
+              : walletExpesseTotal
+                .reduce((acc, curr) => acc + Number(curr.valueSpend), 0)}
+          </span>
           {' '}
           <span data-testid="header-currency-field">BRL</span>
         </p>
@@ -24,14 +34,19 @@ class Header extends Component {
 
 const mapStateToProps = (state) => ({
   userEmail: state.user.email,
+  walletExpesseTotal: state.wallet.expenses,
 });
 
 Header.propTypes = {
   userEmail: PropTypes.string,
+  walletExpesseTotal: PropTypes.arrayOf(PropTypes.shape({
+    valueSpend: PropTypes.number,
+  })),
 };
 
 Header.defaultProps = {
   userEmail: '',
+  walletExpesseTotal: {},
 };
 
 export default connect(mapStateToProps, null)(Header);
